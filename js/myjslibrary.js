72ecf4432f0fe2b8b7084970b7a62c488a6325a7
polinux87 <alessandro.poletti@gmail.com>
2017-09-11 16:17:18 +0200

    push

--- js/myjslibrary.js
+++ js/myjslibrary.js
@@ -129,7 +129,7 @@ window.onclick = function(event) {
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 				if (target.length) {
 				$('html,body').animate({
-				scrollTop: target.offset().top
+				scrollTop: target.offset().top - 50 // per scendere sotto il top menu
 				}, 1000);
 				return false;
 				}
